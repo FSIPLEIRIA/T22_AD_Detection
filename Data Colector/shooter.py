09c5ff4c6e046a3dc15efeb2a56afd1eb738cb75
Daniel Batista <2171836@my.ipleiria.pt>
2021-12-03 23:21:13 +0000

    adding new .gitignore

---
+++ Data Colector/shooter.py
@@ -0,0 +1,35 @@
+import cv2.cv2
+import tensorflow as tf
+import cv2 as cv
+import numpy as np
+import keras as kr
+import sys
+import time
+from datetime import datetime
+import realsense2 as rs
+
+
+
+
+def start():
+    cv.cv2.VideoCapture(0)
+
+    pass
+
+
+
+def loop():
+    cv.cv2
+
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    cv.cv2.imwrite(current_time+".png",)
+
+    now = None
+    pass
+
+
+if __name__ == '__main__':
+    start()
+    while True:
+        loop()
